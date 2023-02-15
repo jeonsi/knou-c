fa5358c541c8dbbbd1ea69a84f0d97b0ada99922
Seung-il Jeon <si.j@navercorp.com>
2023-02-15 11:37:31 +0900

    length of variable types

---
+++ ex2-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main() {
+    printf("%d bytes char\n", sizeof(char));
+    printf("%d bytes signed char\n", sizeof(signed char));
+    printf("%d bytes unsigned char\n", sizeof(unsigned char));
+    printf("%d bytes short\n", sizeof(short));
+    printf("%d bytes short int\n", sizeof(short int));
+    printf("%d bytes signed short\n", sizeof(signed short));
+    printf("%d bytes signed short int\n", sizeof(signed short int));
+    printf("%d bytes unsigned short\n", sizeof(unsigned short));
+    printf("%d bytes unsigned short int\n", sizeof(unsigned short int));
+    printf("%d bytes int\n", sizeof(int));
+    printf("%d bytes signed int\n", sizeof(signed int));
+    printf("%d bytes unsigned\n", sizeof(unsigned));
+    printf("%d bytes unsigned int\n", sizeof(unsigned int));
+    printf("%d bytes long\n", sizeof(long));
+    printf("%d bytes long int\n", sizeof(long int));
+    printf("%d bytes signed long\n", sizeof(signed long));
+    printf("%d bytes signed long int\n", sizeof(signed long int));
+    printf("%d bytes unsigned long\n", sizeof(unsigned long));
+    printf("%d bytes unsigned long int\n", sizeof(unsigned long int));
+    printf("%d bytes long long\n", sizeof(long long));
+    printf("%d bytes long long int\n", sizeof(long long int));
+    printf("%d bytes signed long long\n", sizeof(signed long long));
+    printf("%d bytes signed long long int\n", sizeof(signed long long int));
+    printf("%d bytes unsigned long long\n", sizeof(unsigned long long));
+    printf("%d bytes unsigned long long int\n", sizeof(unsigned long long int));
+    printf("%d bytes float\n", sizeof(float));
+    printf("%d bytes double\n", sizeof(double));
+    printf("%d bytes long double\n", sizeof(long double));
+}
