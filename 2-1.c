c39e1e6e44e9615971d8e2675136698d942e38c7
Seung-il Jeon <si.j@navercorp.com>
2023-02-23 10:49:12 +0900

    add comment

--- 2-1.c
+++ 2-1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main() {
-    int a, b;
-    a = 100;
-    b = 50;
+    int a, b;   // 변수 선언
+    a = 100;    // 변수 사용, 변수 a에 100을 대입
+    b = 50;     // 변수 사용, 변수 b에 50을 대입
     printf("a = %d, b = %d\n", a, b);
 }
--- 2-2.c
+++ 2-2.c
@@ -12,9 +12,9 @@ void main() {
     long long ll1 = LLONG_MIN;
     long long ll2 = LLONG_MAX;
 
-    printf("char 자료형 범위: %d ~ %d\n", ch1, ch2);
-    printf("short 자료형 범위: %d ~ %d\n", sh1, sh2);
-    printf("int 자료형 범위: %d ~ %d\n", in1, in2);
-    printf("long 자료형 범위: %ld ~ %ld\n", lo1, lo2);
+    printf("char 자료형 범위     : %d ~ %d\n", ch1, ch2);
+    printf("short 자료형 범위    : %d ~ %d\n", sh1, sh2);
+    printf("int 자료형 범위      : %d ~ %d\n", in1, in2);
+    printf("long 자료형 범위     : %ld ~ %ld\n", lo1, lo2);
     printf("long long 자료형 범위: %lld ~ %lld\n", ll1, ll2);
 }
