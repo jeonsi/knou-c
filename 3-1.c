e691980f94d282935187ea72c340db943bc0064e
Seung-il Jeon <si.j@navercorp.com>
2023-02-27 11:37:42 +0900

    ch03 more coding

---
+++ 3-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+    char c = 'A';
+    int i = 10, j = 20, k = 30;
+    // print()문의 인용부호 내 문자가 화면에 출력됨
+    printf("간단한 출력 프로그램\n");
+    // 변수 c는 순서대로 %c와 %d에 대음됨
+    printf("c = %c, c의 아스키 코드값은 %d\n", c, c);
+    // 변수 i, j, k는 순서대로 %문자와 대응됨
+    printf("i = %d, j = %d, k = %d\n", i, j, k);
+}
---
+++ 3-2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main() {
+    printf("%%d : %d\n", 2147483647);
+    printf("%%ld : %ld\n", 2147483647);
+
+}
